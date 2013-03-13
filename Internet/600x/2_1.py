c86a19955ef91127d6ae0324a1dfa91012a768de
Vicki Crosson <viccro@Mirinyc.(none)>
2013-03-13 07:18:41 -0400

    Int2

---
+++ Internet/600x/2_1.py
@@ -0,0 +1,16 @@
+annualInterestRate = 1
+balance = 2
+monthlyPaymentRate = 3
+
+monthlyInterestRate = annualInterestRate / 12.
+month = 0
+totalPaid = 0
+
+while (month < ):
+	#compute new monthly payment
+	#update balance by subtracting that payment, and adding monthly interest
+	#output the shit
+	#month ++
+	#totalpaid ++
+
+#print total amt paid
